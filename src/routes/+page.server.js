b31b316aacafcda9e721e3882532e8af6cea1db3
CrzyMan <tywhitehouse10@hotmail.com>
2025-01-30 11:34:32 -0500

    try importing zlib directly

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,14 +1,46 @@
 import { brotliDecompressSync } from "zlib";
 
 /**
+ * @typedef {Object} MyError
+ * @property {string} code
+ * @property {string} message
+ * @property {string} [detail]
+ * @property {string} [suggestion]
+ * @property {MyError} [stack]
+ */
+
+/**
+ * @typedef {null | undefined} Nullish
+ */
+
+/**
+ @template T
+ @typedef {
+    {data: T, error: Nullish} |
+    {data: Nullish, error: MyError}
+ } AttemptResponse
+ */
+
+/**
  * @param {string} compressed_string
+ * @returns {AttemptResponse<string>}
  */
 function decompress(compressed_string) {
-    const textDecoder = new TextDecoder();
-    const compressed_buffer_from_string = compressed_string_to_buffer(compressed_string);
-    const decompressedData = brotliDecompressSync(compressed_buffer_from_string);
-    const decompressed_string = textDecoder.decode(decompressedData);
-    return decompressed_string;
+    try {
+        const textDecoder = new TextDecoder();
+        const compressed_buffer_from_string = compressed_string_to_buffer(compressed_string);
+        const decompressedData = brotliDecompressSync(compressed_buffer_from_string);
+        const decompressed_string = textDecoder.decode(decompressedData);
+        return { data: decompressed_string, error: null };
+    } catch (e) {
+        return {
+            data: null,
+            error: {
+                code: "invalid_data",
+                message: `Could not decompress string`,
+            },
+        };
+    }
 }
 
 /**
@@ -20,15 +52,32 @@ function compressed_string_to_buffer(str) {
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ url }) {
-    let trigger_definitions = `// Comments
+    let default_trigger_definitions = `// Comments
 prop/path/1 -> prop/path/3 /* inline comments */
 prop/path/1 -> prop/path/2 : relationship`;
 
     let compressed = url.searchParams.get("r");
-    if (compressed) {
-        trigger_definitions = decompress(compressed);
+
+    if (!compressed) {
+        return { 
+            trigger_definitions: default_trigger_definitions 
+        };
     }
+
+    let { data: decompressed_data, error: decompression_error } = decompress(compressed);
+
+    if (decompression_error){
+        return {
+            trigger_definitions: default_trigger_definitions, 
+            error: {
+                code: 'invalid_search',
+                message: `Could not parse the URL into trigger definitions`,
+                stack: decompression_error
+            }
+        };
+    }
+
     return {
-        trigger_definitions,
+        trigger_definitions: decompressed_data,
     };
 }
